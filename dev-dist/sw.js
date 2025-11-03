57ad998aee0c6feef52ff44e03c8d0b4db261b6f
linjialiang <linjialiang@163.com>
2025-11-03 14:40:03 +0800

    update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c6a197bf'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.hieu5bpgvk"
+    "revision": "0.cd46k1rh6kg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
